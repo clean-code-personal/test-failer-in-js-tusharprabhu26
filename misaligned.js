8c9c29f5ce3f3e7a581c1883f6f2b66b52fca0fe
Tushar Prabhu <tusharprabhu26@gmail.com>
2024-02-08 04:56:45 +0530

    test: Add failing test with respect to misalignments
    Changed variable, function names to make it easier for customers to understand the purpose.
    Refactored the `generateColorCombinations` function to return the color combinations instead of logging them by moving the `console.log` statement outside the `generateColorCombinations` function.
    Added failing test cases to check for misalignments.

--- misaligned.js
+++ misaligned.js
@@ -1,16 +1,41 @@
-const {expect} = require('chai')
+import { expect } from "chai";
 
-function print_color_map() {
-    const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
-    const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
-    for (let i = 0; i < majorColors.length; i++) {
-        for (let j = 0; j < minorColors.length; j++) {
-            console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
-        }
+function generateColorCombinations() {
+  const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
+  const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
+  let colorCombinationsList = [];
+  for (let majorIndex = 0; majorIndex < majorColors.length; majorIndex++) {
+    for (let minorIndex = 0; minorIndex < minorColors.length; minorIndex++) {
+      let colorCombination = `${majorIndex * 5 + minorIndex} | ${
+        majorColors[majorIndex]
+      } | ${minorColors[minorIndex]}`;
+      colorCombinationsList.push(colorCombination);
     }
-    return majorColors.length * minorColors.length;
+  }
+  return colorCombinationsList;
 }
 
-result = print_color_map();
-expect(result).equals(25);
-console.log('All is well (maybe!)');
+let colorCombinationsResult = generateColorCombinations();
+
+// Capture console.log output
+let loggedOutput = [];
+console.log = (output) => loggedOutput.push(output);
+
+colorCombinationsResult.forEach((colorCombination) =>
+  console.log(colorCombination)
+);
+
+expect(loggedOutput.length).equals(25);
+
+// Test for misalignments
+loggedOutput.forEach((colorCombination) => {
+  let colorParts = colorCombination.split("|");
+
+  expect(colorParts[0].length).equals(4); // This will fail if the numeric value is not aligned with the separator
+
+  expect(colorParts[1].length).equals(7); // This will fail iif the majorColor is not aligned with the separator
+  
+  expect(colorParts[2].length).equals(7); // This will fail if the minorColor is not aligned with the separator
+});
+
+console.log("All is well (maybe!)");
