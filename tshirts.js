92d126e1f0bb128a3fed28701972f7aae102f56d
Tushar Prabhu <tusharprabhu26@gmail.com>
2024-02-07 12:30:21 +0530

    test: Add failing test for size(cms) function

--- tshirts.js
+++ tshirts.js
@@ -13,4 +13,5 @@ function size(cms) {
 expect(size(37)).equals('S');
 expect(size(40)).equals('M');
 expect(size(43)).equals('L');
+expect(size(38)).equals('M'); // This will fail because size(38) returns 'L'
 console.log('All is well (maybe!)');
