16a5c77ab6046f811a258b36a627a8216e9316e1
Tushar Prabhu <tusharprabhu26@gmail.com>
2024-02-08 05:04:44 +0530

    refactor: Separate test code from function implementation for misaligned.js

--- misaligned.js
+++ misaligned.js
@@ -1,6 +1,4 @@
-import { expect } from "chai";
-
-function generateColorCombinations() {
+export function generateColorCombinations() {
   const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
   const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
   let colorCombinationsList = [];
@@ -14,28 +12,3 @@ function generateColorCombinations() {
   }
   return colorCombinationsList;
 }
-
-let colorCombinationsResult = generateColorCombinations();
-
-// Capture console.log output
-let loggedOutput = [];
-console.log = (output) => loggedOutput.push(output);
-
-colorCombinationsResult.forEach((colorCombination) =>
-  console.log(colorCombination)
-);
-
-expect(loggedOutput.length).equals(25);
-
-// Test for misalignments
-loggedOutput.forEach((colorCombination) => {
-  let colorParts = colorCombination.split("|");
-
-  expect(colorParts[0].length).equals(4); // This will fail if the numeric value is not aligned with the separator
-
-  expect(colorParts[1].length).equals(7); // This will fail iif the majorColor is not aligned with the separator
-  
-  expect(colorParts[2].length).equals(7); // This will fail if the minorColor is not aligned with the separator
-});
-
-console.log("All is well (maybe!)");
