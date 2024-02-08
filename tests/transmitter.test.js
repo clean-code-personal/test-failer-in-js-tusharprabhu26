c5c012f3701837580617dc58386184e8fdb426db
Tushar Prabhu <tusharprabhu26@gmail.com>
2024-02-08 05:49:18 +0530

    Add failing tests to catch transmissionFailureCount bug

---
+++ tests/transmitter.test.js
@@ -0,0 +1,30 @@
+import { expect } from "chai";
+import { transmitInCelcius, transmissionFailureCount } from "../transmitter.js";
+
+function testTransmitInCelcius() {
+  // Temperature above 100 celcius
+  transmitInCelcius(400.5);
+  expect(transmissionFailureCount).to.equal(
+    1,
+    "Transmission failure count did not increment"
+  );
+
+  // Temperature above 100 celcius 
+  transmitInCelcius(303.6);
+  expect(transmissionFailureCount).to.equal(
+    1,
+    "Transmission failure count did not increment"
+  );
+
+  // Temperature exactly 100 celcius
+  transmitInCelcius(212);
+  expect(transmissionFailureCount).to.equal(
+    1,
+    "Transmission failure count did not increment"
+  );
+}
+
+testTransmitInCelcius();
+
+console.log(`Transmission failed ${transmissionFailureCount} times.`);
+console.log("All is well (maybe!)");
