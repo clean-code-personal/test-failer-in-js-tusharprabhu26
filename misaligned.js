d6fc654a06c2cfbf2eaee93b4e50f76ea5023c3f
Tushar Prabhu <tusharprabhu26@gmail.com>
2024-02-08 15:35:19 +0530

    refactor: Isolate the number-calculation to a separate function

--- misaligned.js
+++ misaligned.js
@@ -1,12 +1,15 @@
+export function generateColorNumber(majorIndex, minorIndex) {
+  return majorIndex * 5 + minorIndex;
+}
+
 export function generateColorCombinations() {
   const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
   const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
   let colorCombinationsList = [];
   for (let majorIndex = 0; majorIndex < majorColors.length; majorIndex++) {
     for (let minorIndex = 0; minorIndex < minorColors.length; minorIndex++) {
-      let colorCombination = `${majorIndex * 5 + minorIndex} | ${
-        majorColors[majorIndex]
-      } | ${minorColors[minorIndex]}`;
+      let colorNumber = generateColorNumber(majorIndex, minorIndex);
+      let colorCombination = `${colorNumber} | ${majorColors[majorIndex]} | ${minorColors[minorIndex]}`;
       colorCombinationsList.push(colorCombination);
     }
   }
