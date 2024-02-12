03808055dd3720bbede27164b8420b19326c1470
Tushar Prabhu <tusharprabhu26@gmail.com>
2024-02-12 07:06:58 +0530

    fix: update size function to return 'S' for 38cm

--- tests/tshirts.test.js
+++ tests/tshirts.test.js
@@ -4,5 +4,5 @@ import size from "../tshirts.js";
 expect(size(37)).equals("S");
 expect(size(40)).equals("M");
 expect(size(43)).equals("L");
-expect(size(38)).equals("M"); // This will fail because size(38) returns 'L'
+expect(size(38)).equals("S");
 console.log("All is well (maybe!)");
