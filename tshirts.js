60794bb65156b5735441f24775fa3d84011757f9
Tushar Prabhu <tusharprabhu26@gmail.com>
2024-02-12 07:05:57 +0530

    fix: adjust size function to correctly categorize 38cm as 'S' instead of 'L'

--- tshirts.js
+++ tshirts.js
@@ -1,9 +1,9 @@
 export default function size(cms) {
-    if (cms < 38) {
-        return 'S';
-    } else if (cms > 38 && cms < 42) {
-        return 'M';
-    } else {
-        return 'L';
-    }
+  if (cms <= 38) {
+    return "S";
+  } else if (cms > 38 && cms < 42) {
+    return "M";
+  } else {
+    return "L";
+  }
 }
